d556c8356b2b1cfa0c3dc876e992a439c7051bd2
knup711 <127697480+knup711@users.noreply.github.com>
2023-10-26 21:04:52 +0200

    add buttons to change the grid size

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const spielfeld = document.querySelector("div");
+const spielfeld = document.querySelector(".spielfeld");
+let times;
 
-function createGameField(times) {
+function createGame(times) {
     let column;
     let row;
+    while (spielfeld.firstChild) {
+        spielfeld.removeChild(spielfeld.lastChild);
+      }
     for (i = 0; i < times; i++) {
         row = document.createElement("div");
         row.classList.add("row");
         spielfeld.append(row);
         for (j = 0; j < times; j++) {
-        column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
+            column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
         }
     }
+    getPainted();
 };
 
 function getPainted (event) {
     let painted = document.querySelectorAll(".column");
-
     for (i = 0; i < painted.length; i++) {
     painted[i].addEventListener ("mouseover", (event) => {
         event.target.style.backgroundColor = "black";
+        event.target.style.borderColor = "black";
     });
 };
 };
 
-
-createGameField(16);
-getPainted();
-
+createGame(16);
